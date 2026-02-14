22e36b02750b59a67ed7e19cf49cd446e0fdf470
judai39 <dhy2522609443@163.com>
2026-02-14 15:57:13 +0800

    asyncio相关关键字

---
+++ 23_异步io/2_asyncio/1_asyncio_and_await.py
@@ -0,0 +1,12 @@
+# asyncio是 Python 中用于编写异步代码的标准库，它提供了一组工具和API来管理和调度协程。通过asyncio，可以轻松创建、执行和
+# 管理异步任务。
+# 是异步并发的
+import asyncio
+
+# async关键字用于定义异步函数
+async def asyncio_function():
+    # await关键字用于暂停异步函数的执行，等待另一个异步任务完成。
+    await asyncio.sleep(1)
+    print("asyncio function executed")
+
+asyncio.run(asyncio_function())
